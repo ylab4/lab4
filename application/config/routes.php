4e5a915a2c0f52de2c7e5f9e986142f0f6985985
Yen Bui <ybui0210@gmail.com>
2018-01-30 19:46:44 -0800

    closed #2 #3 #4 #5

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+$route['bravo'] = "bravo/welcome/index";
+$route['echo'] = "echo/must/wehave";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
